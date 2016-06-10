5738b2b73f80ffb2370e8c33f753310db08163bd
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2016-06-10 15:38:52 -0500

    Initialize context, draw rectangle

--- stompy.js
+++ stompy.js
@@ -1,7 +1,11 @@
 var canvas;
+var ctx;
 
 function init() {
     canvas = document.getElementById('canvas');
     canvas.width = 600;
     canvas.height = 600;
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'white';
+    ctx.fillRect(50, 100, 400, 200);
 }
