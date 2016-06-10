2387160d6381005e1cf09fec29466dcdc8eff824
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2016-06-10 15:56:26 -0500

    Initialize gameLoop

--- stompy.js
+++ stompy.js
@@ -1,11 +1,24 @@
 var canvas;
 var ctx;
+var pos = {
+    x: 0,
+    y: 0
+};
 
 function init() {
     canvas = document.getElementById('canvas');
     canvas.width = 600;
     canvas.height = 600;
     ctx = canvas.getContext('2d');
+
+    gameLoop();
+}
+
+function gameLoop() {
+    pos.x++;
+    pos.y++;
     ctx.fillStyle = 'white';
-    ctx.fillRect(50, 100, 400, 200);
+    ctx.fillRect(pos.x, pos.y, 50, 50);
+
+    window.requestAnimationFrame(gameLoop);
 }
